2e3d11f80c8ad5d21b97f53c730958fb1b076c27
Arief Ramadhan <arief25ramadhan@yahoo.com>
2018-10-05 07:17:28 +0100

    Add descriptive comments to designsAR.js
    Descriptive comments that explain the purpose of each lines.

--- designsAR.js
+++ designsAR.js
@@ -3,36 +3,43 @@ Udacity Pixel Art Maker Project
 By: Arief Ramadhan
 */
 
-var tableForm = document.querySelector('#pixelCanvas');
-var submitForm = document.querySelector('#sizePicker');
-var clearAll = document.querySelector('#clearAll');
+var tableForm = document.querySelector('#pixelCanvas'); //Select a table element with ID pixelCanvas
+var submitForm = document.querySelector('#sizePicker'); //Select a form element with ID sizePicker
+var clearAll = document.querySelector('#clearAll'); //Select a button element with ID clearAll
 
+//Add an event listener to the submitForm
 submitForm.addEventListener('submit',function(event){
-  event.preventDefault();
+  event.preventDefault(); //Prevent default action
+  //Select input sizes
   var height = submitForm.querySelector('#inputHeight').value;
   var width = submitForm.querySelector('#inputWidth').value;
-  makeGrid(height, width);
+  makeGrid(height, width);//Generate a table using the submitted input sizes as parameters
 })
 
+//Function to make the table blocks (rows x cells)
 function makeGrid(n, m) {
 
-  tableForm.innerHTML = '';
-
+  tableForm.innerHTML = ''; //Clear table
+  
+  //Create rows based on input height
   for (var i = 0; i < n; i++){
     var row = tableForm.insertRow(i);
+    //Create cells based on input width
     for(var j = 0; j < m; j++){
       var cell = row.insertCell(j);}
-
+    
+  //Add event listener to the generated table blocks
   row.addEventListener('click', function(event){
-    var color = document.querySelector('#colorPicker').value;
-    event.target.style.backgroundColor = color;
+    var color = document.querySelector('#colorPicker').value; //Select color input
+    event.target.style.backgroundColor = color; //Implement the color to the clicked table block
   })
-
-  tableForm.appendChild(row);
+  //
+  tableForm.appendChild(row); //Add the generated table blocks to the table
   }
 }
 
-clearAll.addEventListener('click', function(){
-  while (tableForm.firstChild){
-    tableForm.firstChild.remove();}
+//Add event listener to the clearAll button 
+clearAll.addEventListener('click', function(){ //If the clearAll button is clicked
+  while (tableForm.firstChild){ //While table block exists
+    tableForm.firstChild.remove();} //Remove table block
   })
